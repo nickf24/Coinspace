9fdec27d1894cb5c1a3cb9662c18a99add8c638f
Nick Forster <nforster24@gmail.com>
2018-01-17 14:54:31 -0800

    exchange forms complete

--- client/src/components/BuyCard.jsx
+++ client/src/components/BuyCard.jsx
@@ -8,10 +8,53 @@ class BuyCard extends React.Component {
   	super(props)
   }
 
+
   render() {
+
   	return (
 
-  		<div id="dashCard" className="ui blue raised card"> This is a buy card </div>
+  		<div id="dashCard3" className="ui blue raised card"> 
+  		  <form id = 'buyForm' className = 'ui mini form'>
+  		    <fieldset>
+  		  	  <h3> Buy Bitcoin </h3>
+			  <div id = 'availableBalance' className = 'field'>
+	  		    <h5> Available USD: </h5>
+	  		    <span> $100,000 </span>
+	  		  </div>
+	  		  <div className = "field">
+	  		    <h5> Order Type: </h5>
+	      		<div className ="ui radio checkbox">
+	        		<input type="radio" name="market"/>
+	        		<label>Market</label>
+	      		</div>
+
+	      		&nbsp;
+	      		&nbsp;
+	      	    <div class="ui radio checkbox">
+	        	  <input type="radio" name="market"/>
+	        	  <label>Limit</label>
+	      	    </div>
+	    	  </div>
+
+	  		  <div id = 'selectVolume' className = 'field'>
+	  		  	<h5> Volume: </h5>
+	  		  	<input type = 'text' id = 'buyVolume' placeholder = 'amount to receive'/>
+	  		  </div>
+	  		  <div id = 'selectPrice' className = 'field'>
+	  		  	<h5> Price: </h5>
+	  		  	<input type = 'text' id = 'buyPrice'/> 
+	  		  </div>
+	  		  <div id = 'totalAmount' className = 'field'>
+	  		  	<h5> Spend Total: </h5>
+	  		  	<span id = 'totalBuyOrder'> - </span>
+	  		  </div>
+	  		  <div id = 'submitOrder'>
+	  		  	<button className = 'ui primary button'> Buy ETH </button>
+	  		  </div>
+	  		</fieldset>
+  		  </form> 
+
+  		</div>
   	)
   }
 
